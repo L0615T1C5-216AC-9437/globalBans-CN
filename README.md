4c553c8551f5ab3c0d27ce9f40694e9b4f92e930
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2021-04-03 03:31:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Discord](https://img.shields.io/discord/519293558599974912.svg)](http://cn-discord.ddns.net)
+### Installing
+
+1) Place the latest `.jar` file found in [releases](https://github.com/L0615T1C5-216AC-9437/globalBans-CN/releases) into your mods folder.
+2) Send your domain name to [L0615T1C5.216AC#9437](http://cn-discord.ddns.net) in order to whitelist your ip
+3) Reload the server and enable debug if you encounter any issues
---
+++ src/gb/main.java
@@ -0,0 +1,56 @@
+package gb;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Packets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+public class main extends Plugin{
+    public static HashMap<String, Long> cache = new HashMap<>();
+    //called when game initializes
+    @Override
+    public void init(){
+        Events.on(EventType.PlayerJoin.class, event -> {
+            Player player = event.player;
+            try {
+                HttpURLConnection con = (HttpURLConnection) new URL("http://chaotic-neutral.ddns.net:8080/badListData?uuidip="+player.uuid()).openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                if (con.getResponseCode() == 200) {
+                    String[] data = response.toString().substring(1, response.toString().length() - 2).split(",");
+                    if (!data[1].contains("null")) {
+                        long kick = Strings.parseLong(data[1].substring(data[1].indexOf(":")+1), 0);
+                        if (kick > System.currentTimeMillis()) {
+                            player.con.kick(Packets.KickReason.recentKick);
+                            return;
+                        }
+                    }
+                    if (!data[2].contains("null") && !data[2].contains("false"))
+                        player.con.kick(Packets.KickReason.recentKick);
+                } else {
+                    Log.debug("State:"+con.getResponseCode()+" - "+response.toString());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
